4ce6c3aac81def54f3d41a992890bdf5ed9fd1b4
ES47989768S <ES47989768S@enel.com>
2020-10-25 18:44:55 +0100

    Added camera following player through network manager. Added 4 spawns, one for each player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Cinemachine;
+
+public class CameraController : NetworkBehaviour
+{ 
+    [Header("Camera")]
+    [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
+
+    private CinemachineTransposer transposer;
+
+    public override void OnStartAuthority()
+    {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+        virtualCamera.gameObject.SetActive(true);
+        
+        enabled = true;
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    }*/
+
+}
---
+++ Assets/Scripts/NetworkManagerPossession.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManagerPossession : NetworkManager
+{
+
+    public Transform Spawn_1;
+    public Transform Spawn_2;
+    public Transform Spawn_3;
+    public Transform Spawn_4;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+
+        Transform start;
+        GameObject player;
+
+        switch (numPlayers)
+        {
+            case 0:
+                start = Spawn_1;
+                break;
+            case 1:
+                start = Spawn_2;
+                break;
+            case 2:
+                start = Spawn_3;
+                break;
+            case 3:
+                start = Spawn_4;
+                break;
+
+            default:
+                start = Spawn_1;
+                break;
+        }
+
+        player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
+    }
+}
