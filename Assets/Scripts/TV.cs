48bac859b673df1d0351d793eff7bdd9fea786ab
ES47989768S <ES47989768S@enel.com>
2020-11-01 19:30:35 +0100

    Added interactable objects

---
+++ Assets/Scripts/TV.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TV : MonoBehaviour
+{
+
+    public bool playerInRange;
+    public BoxCollider2D collider2DD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInRange = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("interact") && playerInRange)
+        {
+            this.GetComponent<TVBehaviour>().TurnOnOff();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
+            playerInRange = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TVBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVBehaviour : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOnOff()
+    {
+
+        if(animator.GetBool("on"))
+        {
+            animator.SetBool("on", false);
+        } else
+        {
+            animator.SetBool("on", true);
+        }
+    }
+
+}
