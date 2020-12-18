b127c6ddad2d66d31a2482efcd9335097b30dbd7
ES47989768S <ES47989768S@enel.com>
2020-12-18 21:06:15 +0100

    Added main menú

---
+++ Assets/Scripts/Main menu/NetworkManagerPossessionHUD.cs
@@ -0,0 +1,31 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerPossessionHUD : MonoBehaviour
+{
+
+    NetworkManagerPossession manager;
+    public TMP_InputField ipInput;
+    public 
+
+    void Awake()
+    {
+        manager = GetComponent<NetworkManagerPossession>();
+    }
+
+    public void OnHost()
+    {
+        manager.StartHost();
+    }
+
+    public void OnConnectAsClient()
+    {
+        manager.networkAddress = ipInput.text;
+        manager.StartClient();
+    }
+
+}
